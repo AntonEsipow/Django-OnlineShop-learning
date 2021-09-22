10d051c866c3b9680324a38e3bd62fa99f2a92db
AntonEsipow <esipow.anton99@gmail.com>
2021-09-22 20:53:57 +0300

    Test gitignore

--- myshop/myshop/settings.py
+++ myshop/myshop/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+from decouple import config
 from pathlib import Path
 import os
 
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=2l=!kep#*#tm%5jmm-#tj8kv2r1k++4%yzsi8gexm1b8rv2)n'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
